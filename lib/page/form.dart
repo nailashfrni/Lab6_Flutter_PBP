13f0b6f1aa61dfda79c332d0b63721d06f400312
nailashfrni <naila.shafirni@ui.ac.id>
2022-11-17 09:14:07 +0700

    Lab 8 done

--- lib/page/form.dart
+++ lib/page/form.dart
@@ -1,4 +1,5 @@
 import 'package:lab_flutter/main.dart';
+import 'package:lab_flutter/page/to_do_page.dart';
 import 'package:flutter/material.dart';
 
 class MyFormPage extends StatefulWidget {
@@ -68,6 +69,16 @@ class _MyFormPageState extends State<MyFormPage> {
                           MaterialPageRoute(builder: (context) => const MyFormPage()),
                         );
                       },
+                    ),
+                    ListTile(
+                        title: const Text('To Do'),
+                        onTap: () {
+                            // Route menu ke halaman to do
+                            Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(builder: (context) => const ToDoPage()),
+                            );
+                        },
                     ),
                   ],
                 ),
